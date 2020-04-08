734e0c43a47360486d82aeb1de276b21d2cb0e9b
crimsoncrow <mors@void>
2020-04-08 14:04:46 +0800

    added noracodes/crackme05e_keygen.py

---
+++ noracodes/crackme05e_keygen.py
@@ -0,0 +1,67 @@
+import random
+
+def part1():
+    while True:
+        p = ""
+        s = 0
+        p += random.choice(ch)
+        p += random.choice(ch)
+        p += 'm'
+        p += random.choice(ch)
+        for c in p:
+            s += ord(c)
+        if s % 2 == 0:
+            return p
+
+def part2():
+    while True:
+        p = ""
+        s = 0
+        p += random.choice(ch)
+        p += random.choice(ch)
+        p += random.choice(ch)
+        p += random.choice(ch)
+        for c in p:
+            s += ord(c)
+        if s % 3 == 0:
+            return p
+
+def part3():
+    while True:
+        p = ""
+        s = 0
+        p += random.choice(ch)
+        p += random.choice(ch)
+        p += '\''
+        p += random.choice(ch)
+        for c in p:
+            s += ord(c)
+        if s % 5 == 0:
+            return p
+
+def part4():
+    while True:
+        p = ""
+        s = 0
+        p += random.choice(ch)
+        p += random.choice(ch)
+        p += random.choice(ch)
+        p += random.choice(ch)
+        for c in p:
+            s += ord(c)
+        if s % 2 == 0:
+            return p
+
+
+def keygen():
+    while True:
+        p1 = part1()
+        p2 = part2()
+        p3 = part3()
+        p4 = part4()
+        key = p1+p2+p3+p4
+        print (key)
+
+
+ch = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+keygen()
